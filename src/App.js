9cbe82df34328f903dbae97ffb000339c67ef49b
pinglu85 <luping8576@hotmail.com>
2020-01-09 06:23:32 +0100

    add sections and features component

--- src/App.js
+++ src/App.js
@@ -2,23 +2,26 @@ import React from 'react';
 import Rent from './Rent/Rent';
 import Capital from './Capital/Capital';
 import Invest from './Invest/Invest';
+import Features from './Features/Features';
 import appStyles from './App.module.css';
 
 function App() {
   return (
     <div className={appStyles.App}>
-      <div className={appStyles.AnimationWrapper}>
-        <p>Rent 1</p>
-        <Rent />
-      </div>
-      <div className={appStyles.AnimationWrapper}>
-        <p>Capital 1</p>
-        <Capital />
-      </div>
-      <div className={appStyles.AnimationWrapper}>
-        <p>Invest</p>
-        <Invest />
-      </div>
+      <section className={appStyles.Section1}>
+        <div className={appStyles.AnimationWrapper}>
+          <Rent />
+        </div>
+        <div className={appStyles.AnimationWrapper}>
+          <Capital />
+        </div>
+        <div className={appStyles.AnimationWrapper}>
+          <Invest />
+        </div>
+      </section>
+      <section>
+        <Features />
+      </section>
     </div>
   );
 }
