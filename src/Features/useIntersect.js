8ceb7157f9fc583160ae1f1b5d8de954492e2cab
pinglu85 <luping8576@hotmail.com>
2020-01-11 11:38:00 +0100

    move useRef and intersection observer callback to component, remove entry state to avoid that entry is an empty object at the begining

--- src/Features/useIntersect.js
+++ src/Features/useIntersect.js
@@ -1,17 +1,27 @@
-import { useState, useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import 'intersection-observer';
 
-export default function useIntersect() {
-  const ref = useRef();
-  const [entry, updateEntry] = useState({});
-  const [io] = useState(
-    () => new window.IntersectionObserver(([entry]) => updateEntry(entry))
-  );
+export default function useIntersect({ target, onIntersect }) {
   useEffect(() => {
-    const node = ref.current;
+    const io = new window.IntersectionObserver(onIntersect);
+    const node = target.current;
     io.POLL_INTERVAL = 100;
     io.observe(node);
     return () => io.unobserve(node);
-  }, [io]);
-  return [ref, entry];
+  }, [target, onIntersect]);
 }
+
+// export default function useIntersect() {
+//   const ref = useRef(null);
+//   const [entry, updateEntry] = useState({});
+//   const [io] = useState(
+//     () => new window.IntersectionObserver(([entry]) => updateEntry(entry))
+//   );
+//   useEffect(() => {
+//     const node = ref.current;
+//     io.POLL_INTERVAL = 100;
+//     io.observe(node);
+//     return () => io.unobserve(node);
+//   }, [io]);
+//   return [ref, entry];
+// }
