4adba5c43dd20c6215a9ccee267eafd9e98a3149
pinglu85 <luping8576@hotmail.com>
2020-01-15 11:00:27 +0100

    rewrite useIntersect to fix improper behavior

--- src/Features/useIntersect.js
+++ src/Features/useIntersect.js
@@ -1,27 +1,39 @@
-import { useEffect } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import 'intersection-observer';
 
-export default function useIntersect({ target, onIntersect }) {
+export default function useIntersect({
+  triggerOnce,
+  root = null,
+  rootMargin,
+  threshold
+}) {
+  const ref = useRef(null);
+  const [state, setState] = useState({
+    inView: false,
+    entry: undefined
+  });
+  const [io] = useState(
+    () =>
+      new IntersectionObserver(
+        ([entry]) => {
+          setState({
+            entry: entry
+          });
+          if (entry.isIntersecting) {
+            setState({ inView: true });
+          }
+          if (entry.isIntersecting && triggerOnce) {
+            io.unobserve(entry.target);
+          }
+        },
+        { root, rootMargin, threshold }
+      )
+  );
   useEffect(() => {
-    const io = new IntersectionObserver(onIntersect);
-    const node = target.current;
+    const node = ref.current;
     io.POLL_INTERVAL = 100;
     io.observe(node);
     return () => io.unobserve(node);
-  }, [target, onIntersect]);
+  }, [io]);
+  return [ref, state.inView, state.entry];
 }
-
-// export default function useIntersect() {
-//   const ref = useRef(null);
-//   const [entry, updateEntry] = useState({});
-//   const [io] = useState(
-//     () => new window.IntersectionObserver(([entry]) => updateEntry(entry))
-//   );
-//   useEffect(() => {
-//     const node = ref.current;
-//     io.POLL_INTERVAL = 100;
-//     io.observe(node);
-//     return () => io.unobserve(node);
-//   }, [io]);
-//   return [ref, entry];
-// }
