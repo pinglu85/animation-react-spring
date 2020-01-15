6232099b1a5c1df2c8d211241e5a6c0554033ec9
pinglu85 <luping8576@hotmail.com>
2020-01-15 11:16:31 +0100

    update the component to work with the rewritten useIntersect hook

--- src/Features/Features.js
+++ src/Features/Features.js
@@ -1,65 +1,53 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import useIntersect from './useIntersect';
+import Keys from './Keys';
 import featureStyles from './Features.module.css';
 
-const Items = [
-  {
-    id: 'asdw',
-    icon: 'icon1',
-    heading: 'Kostenloser Mietservice',
-    desc:
-      'Lorem ipsum dolor sit amet, consect adipiscing elit ut aliquam, purus sit amet luctus venenatis.'
-  },
-  {
-    id: 'woiu',
-    icon: 'icon2',
-    heading: 'Ideale Kapitalanlage',
-    desc:
-      'Lorem ipsum dolor sit amet, consect adipiscing elit ut aliquam, purus sit amet luctus venenatis.'
-  },
-  {
-    id: 'ioki',
-    icon: 'icon3',
-    heading: 'Investitionsmanagement',
-    desc:
-      'Lorem ipsum dolor sit amet, consect adipiscing elit ut aliquam, purus sit amet luctus venenatis.'
-  }
-];
-
 const Features = () => {
-  const target = useRef(null);
-  const [inViewFromAbove, setInViewFromAbove] = useState(false);
+  const [ref, inView] = useIntersect({ triggerOnce: true, threshold: 0.8 });
 
-  let previousY = 0;
-
-  useIntersect({
-    target,
-    onIntersect: ([entry]) => {
-      const currentY = entry.boundingClientRect.y;
-      const isIntersect = entry.isIntersecting;
-      if (isIntersect) {
-        setInViewFromAbove(true);
-      } else if (currentY > previousY && !isIntersect) {
-        setInViewFromAbove(false); // set to false when scrolling up and not in view
-      }
-      previousY = currentY;
+  const Items = [
+    {
+      id: 'asdw',
+      icon: <Keys triggerAnimation={inView} />,
+      heading: 'Kostenloser Mietservice',
+      desc:
+        'Lorem ipsum dolor sit amet, consect adipiscing elit ut aliquam, purus sit amet luctus venenatis.'
+    },
+    {
+      id: 'woiu',
+      icon: 'icon2',
+      heading: 'Ideale Kapitalanlage',
+      desc:
+        'Lorem ipsum dolor sit amet, consect adipiscing elit ut aliquam, purus sit amet luctus venenatis.'
+    },
+    {
+      id: 'ioki',
+      icon: 'icon3',
+      heading: 'Investitionsmanagement',
+      desc:
+        'Lorem ipsum dolor sit amet, consect adipiscing elit ut aliquam, purus sit amet luctus venenatis.'
     }
-  });
+  ];
+
+  // console.log(inViewFromAbove);
+  // useEffect(() => {
+  //   const currentY = entry.boundingClientRect.y;
+  //   const isIntersect = entry.isIntersecting;
+  //   console.log('previousY:', previousY);
+  //   console.log(currentY);
+  //   console.log('useEffect called');
+  //   if (currentY < previousY && isIntersect) {
+  //     setInViewFromAbove(true);
+  //   }
+  //   setPreviousY(currentY);
+  // }, [entry, previousY, io]);
 
   return (
     <div className={featureStyles.FeaturesWrapper}>
       {Items.map(item => (
-        <div key={item.id}>
-          <div
-            ref={target}
-            style={{
-              backgroundColor: inViewFromAbove ? 'red' : 'grey',
-              transition: 'background-color 1s linear'
-            }}
-            className={featureStyles.FeatureIcon}
-          >
-            {item.icon}
-          </div>
+        <div ref={ref} key={item.id}>
+          <div className={featureStyles.FeatureIcon}>{item.icon}</div>
           <h4 className={featureStyles.FeatureTitle}>{item.heading}</h4>
           <p className={featureStyles.FeatureDesc}>{item.desc}</p>
         </div>
